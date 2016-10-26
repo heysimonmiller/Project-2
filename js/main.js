b91ae37433e001c9c4b54d361bbd9446067c87e4
simonsayswhaat <Simon@Simons-MacBook-Pro.local>
2016-10-26 16:35:08 +0200

    Calculate Page links not working
    Forgot what I changed :D I worked on this a week ago.

--- js/main.js
+++ js/main.js
@@ -12,30 +12,48 @@ email: simonmill@gmail.com
 	//select students and .hide() body <ul class="student-list">
 	document.getElementsByClassName("student-list")[0].style.display = 'none';
 
+//calculate how many page-links are needed
+	//number of students
+	//divided by 10
+	// - 1
+var numStudents = document.getElementsByClassName('student-list')[0].children.length;	//number of students
+var numLinks = Math.ceil(numStudents / 10);												//number of pagination links needed
 
+//testing
+var testingFunction = function() {
+    
+	var test = document.getElementsByClassName('pagination');
+	for (var i = 0; i < test.length; i++) {
+    var a = test[i].getElementsByTagName('a');
+
+	this.className = 'active';
+}; ???????????????????????????????????????????????????
 
 //This function dynamically adds pagination
 	
 function createPagination() {														//start of createPagination() function
 	
-//	for (var i = 0; i < students.length; i++)										//start for loop
 
-		var selectElem = document.getElementsByClassName('page')[0];				//select first element with class 'page'
+	var selectElem = document.getElementsByClassName('page')[0];					//select first element with class 'page'
 		
-		var newDiv = selectElem.appendChild(document.createElement('div'));			//create <div> and append it to element with class='page'
-			newDiv.className = 'pagination';										//set <div>'s class to 'pagination'
+	var newDiv = selectElem.appendChild(document.createElement('div'));				//create <div> and append it to element with class='page'
+		newDiv.className = 'pagination';											//set <div>'s class to 'pagination'
 
-		var newOl = newDiv.appendChild(document.createElement('ol')); 				//create <ol> and append it to <div class='pagination'>	
-		
+	var newOl = newDiv.appendChild(document.createElement('ol')); 					//create <ol> and append it to <div class='pagination'>	
+
+	for (var i = 0; i < numLinks; i++)	{											//start for loop that creates <li>s
 		var newLi = newOl.appendChild(document.createElement('li'));				//create <li> and append it to <ol>
-			newLi.className = 'active';												//set <li>'s class to 'active'
-
+		
 		var newA = newLi.appendChild(document.createElement('a'));					//create <a> and append it to <li>	
-			newA.className = 'active';												//set <a class='active'></a>
+			//newA.className = 'active';											//set <a class='active'></a>
 			newA.href = '#';														//set <a href='#'></a>
-			newA.textContent ='1';													//set <a>1</a>
-
-//	}																				//end for loop
+			newA.textContent = i + 1;												//set <a>1</a> for first link <a>2</a> for second link etc.
+			newA.addEventListener('click', testingFunction);
+	}																				//end for loop
 }																					//end of createPagination() function
 
 createPagination(); 																//call createPagination() function
+/*for (var i = 0; i < numLinks; i++) {
+		document.getElementsByClassName('pagination').removeAttribute("class");
+	}
+	this.className = 'active';*/
